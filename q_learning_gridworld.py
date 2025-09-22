c188c92e31636f70449e306cbcca834a0fd7b805
Taylor <ttallerd@uccs.edu>
2025-09-21 21:20:32 -0600

    Update: Initalized QLearningAgent parameters, added epsilon-greedy, update_q_values, and created training loop Notes: Training loop runs but need to confirm that it is running properly. Visual environment still needs to be added within the Environment class. Plan to go back through the code and do some clean up.

--- q_learning_gridworld.py
+++ q_learning_gridworld.py
@@ -24,6 +24,10 @@ Givens:
 
 # Imported Packages
 import copy
+import random
+import numpy as np
+import pandas as pd
+import warnings
 from frozendict import frozendict
 
 class Environment:
@@ -37,7 +41,7 @@ class Environment:
                 - training_parameters (dict): Contains agent parameters for learning
                 - state_conditions (dict): Contains state positions and condition (dirty/clean)
                 - rendering_parameters (dict): Contains constants for PyGame rendering
-        
+                
         Returns: 
             None
         """
@@ -46,13 +50,11 @@ class Environment:
         self.discount_factor = config["training"]["discount_factor"]
 
         self.initial_state_conditions = frozendict(config["states"])  # This dict does not change (frozendict = immutable)
-        self.state_conditions = copy.deepcopy(self.initial_state_conditions)  # This dict will change so we need a copy
+        self.state_conditions = dict(self.initial_state_conditions)  # This dict will change so we need a copy
 
         self.start_state = (0, 0)
         self.current_state = self.start_state  # initializing the current state = start state
         
-        self.actions = ["up", "down", "right", "left", "clean"]
-    
     def valid_actions(self, state):
         """
         Specifies which actions the agent can choose in a given state.
@@ -125,13 +127,10 @@ class Environment:
                 done (bool): A boolean to indicate if the episode is done (if true - reset) 
         """
 
-        # Grabbing the state condition for the cell the agent is currently in 
-        current_condition = self.state_conditions[self.current_state]
-
         # Defining rewards stucture (cleaning dirty = +10, cleaning clean = -5, moving = 0)
         if action == "clean":
-            if current_condition == "dirty":
-                current_condition = "clean"  # Updating the current state's condition
+            if self.state_conditions[self.current_state] == "dirty":
+                self.state_conditions[self.current_state] = "clean"  # Updating the current state's condition
                 reward = 10
             else:  # Cell is already clean
                 reward = -5
@@ -142,8 +141,8 @@ class Environment:
         next_state = self.next_state(self.current_state, action)
         self.current_state = next_state  # Updating the current state
 
-        # Checking for terminal state (all state_conditions == "clean")
         done = False
+        # Checking for terminal state (all state_conditions == "clean")
         if all(values == "clean" for values in self.state_conditions.values()):
             done = True
 
@@ -163,8 +162,7 @@ class Environment:
 
         # Resetting the current state and the state conditions to initialization value for next episode
         self.current_state = self.start_state
-        for key in self.state_conditions[key]:
-            self.state_conditions[key] = self.initial_state_conditions[key]
+        self.state_conditions = dict(self.initial_state_conditions)
 
         return self.current_state, self.state_conditions
 
@@ -189,46 +187,126 @@ class Environment:
         pass
 
 class QLearningAgent:
-    def __init__(self):
+    def __init__(self, config, actions):
         """
         Initializes agent parameters and Q-Table.
         
         Args:
-        
-        Returns:
-        """
-        pass
+            config (dict): Contains:
+                - training_parameters (dict): Contains agent parameters for learning
+                - state_conditions (dict): Contains state positions and condition (dirty/clean)
+                - rendering_parameters (dict): Contains constants for PyGame rendering
+            actions (dict): All the actions an agent can take within the environment
 
-    def epsilon_greedy(self):
+        Returns: 
+            None
+        """
+        self.learning_rate = config["training"]["learning_rate"]
+        self.discount_factor = config["training"]["discount_factor"]
+        self.epsilon = config["training"]["epsilon"]  # Using e-greedy policy this dictates probability of exploration/exploitation
+        self.states = config["state_to_index"]
+        self.actions = actions
+
+        self.q_table = np.zeros((len(self.states), len(self.actions)), dtype=int)  # This needs to be a matrix of size (# states) x (# actions)
+
+    def epsilon_greedy(self, env, current_state):
         """
         Implements epsilon-greedy policy to dictate the agent's actions.
 
         Args:
+            env (class object): Instance of the environment
+            current_state (tuple): A 2D tuple (x, y) to indicate what the current state is
 
         Returns:
+            chosen_action (str): action chosen for the given state by epsilon_greedy policy
         """
-        pass
+        state_valid_actions = env.valid_actions(current_state)  # Asking environment for the state
+        valid_actions_indices = [self.actions[key] for key in state_valid_actions]
+        state_index = self.states[current_state]
         
-    def update_q_value(self):
+        if random.uniform(0, 1) < self.epsilon:  # This is non-greedy - Explore: random action
+            chosen_action = random.choice(state_valid_actions)
+            return chosen_action
+        
+        else:  # This is the greedy option - Exploit: choose best action
+            q_values = [self.q_table[state_index, action_index] for action_index in valid_actions_indices]
+            best_index = np.argmax(q_values)
+            chosen_action = state_valid_actions[best_index]
+            return chosen_action
+
+    def update_q_value(self, current_state, action, reward, next_state):
         """
         Updates Q-table after agent actions using the Q-function update rule.
         
         Args:
+            current_state (tuple): A 2D tuple (x, y) to indicate what the current state is
+            action (str): Action chosen by the agent
+            next_state (tuple): A 2D tuple (x, y) for the next state given action 
+            reward (int): The benefit/penalty for taking the action that drives agent learning
         
         Returns:
+            None
         """
-        pass
+        state_index = self.states[current_state]
+        next_state_index = self.states[next_state]
+        action_index = self.actions[action]
+
+        # Bellman Optimality Equation
+        self.q_table[state_index, action_index] = self.q_table[state_index, action_index] + self.learning_rate*(reward + self.discount_factor*self.q_table[next_state_index].argmax() - self.q_table[state_index, action_index])
 
 
-def train_agent():
+def train_agent(env, agent, num_episodes):
     """
     Training loop where the agent updates the Q-Table.
     
     Args:
+        env (class object): Environment instance
+        agent (class object): Agent instance
+        num_episodes (int): Total episodes used for training the agent
     
     Returns:
+        training_data.csv: Contains the training data for epsiode, num_steps, episode_reward, and cumulative_reward
     """
-    pass
+
+    # Initializing a Pandas dataframe to log training data
+    #all_episode_data = pd.DataFrame({'Episode': 0, 'Num Steps': 0, 'Episode Reward': 0,
+    #                                 'Cumulative Reward' : 0})
+
+    columns = ['Episode', 'Num Steps', 'Episode Reward', 'Cumulative Reward']
+    all_episode_data = pd.DataFrame()
+
+    cumulative_reward = 0  # Initializing the cumulative reward during training
+
+    # Looping for training episodes 
+    for episode in range(num_episodes):
+
+        current_state, state_conditions = env.reset()  # Resetting the environment at the start of each episode
+        done = False  # Boolean for terminal state (signifies end of episode)
+
+        episode_reward = 0  # Initializing the episode reward during training
+        num_steps = 0  # Intializing a counter for number of steps for an episode
+
+        while not done:
+
+            action = agent.epsilon_greedy(env, current_state)  # Choosing action
+            next_state, reward, done = env.step(action)  # Taking action, observing next state, reward
+            agent.update_q_value(current_state, action, reward, next_state)  # Updating Q-Value
+            current_state = next_state  # Moving to the next state
+
+            episode_reward = episode_reward + reward
+            num_steps = num_steps + 1
+
+        cumulative_reward = cumulative_reward + episode_reward
+
+        episode_data = [episode+1, num_steps, episode_reward, cumulative_reward]
+        episode_data = pd.Series(episode_data)
+
+        # Concatenating this episodes data to the all_episode_data dataframe
+        all_episode_data = pd.concat([all_episode_data, episode_data.to_frame().T], keys=columns, ignore_index=True)
+
+    # Logging all_episode_data to csv file
+    all_episode_data.columns = columns
+    all_episode_data.to_csv('training_data.csv', index=False)
 
 
 def evaluate_agent():
@@ -239,7 +317,7 @@ def evaluate_agent():
     
     Returns:
     """
-    pass
+    print("Still working on...")
 
 
 def configurations():
@@ -257,7 +335,6 @@ def configurations():
 
     # Defining Constants - RL agent
     training_parameters = {
-        "num_episodes": 1000,  # Number of training episodes
         "learning_rate": 0.5,  # alpha
         "discount_factor": 0.9,  # gamma
         "epsilon":  0.8  # used for e-greedy policy (~1 favors exploration and ~0 favors exploitation)
@@ -273,6 +350,21 @@ def configurations():
         (1, 0): "clean",  # State D
     }
 
+    states_to_index = {
+        (0, 1): 0,
+        (1, 1): 1,
+        (0, 0): 2,
+        (1, 0): 3
+    }
+
+    actions = {
+        "up": 0,
+        "down": 1,
+        "right": 2,
+        "left": 3,
+        "clean": 4
+    }
+
     # Defining Constants - PyGame Rendering
     rendering_parameters = {
         "screen_width": 500,
@@ -283,10 +375,11 @@ def configurations():
     config_dict = {
         "training": training_parameters,
         "states": state_conditions,
+        "state_to_index": states_to_index,
         "rendering": rendering_parameters
     }
 
-    return config_dict
+    return config_dict, actions
 
 
 def main():
@@ -298,8 +391,14 @@ def main():
     Returns:
     """
 
-    config = configurations()
+    warnings.filterwarnings("ignore")
+    config, actions = configurations()
+    num_episodes = 1000  # Number of training episodes
     env = Environment(config)
+    agent = QLearningAgent(config, actions)
+
+    # Calling to training loop
+    train_agent(env, agent, num_episodes)
 
 
 if __name__ == "__main__":
